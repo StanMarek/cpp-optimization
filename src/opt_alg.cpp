9002be0b596a8a7d8b46a9474ea1ccb00cc76218
StanMarek <stanislawmarek21@gmail.com>
2021-10-22 00:36:29 +0200

    ToCoNaLabachcos

--- src/opt_alg.cpp
+++ src/opt_alg.cpp
@@ -3,40 +3,53 @@
 double *expansion(double x0, double d, double alpha, int Nmax, matrix *ud, matrix *ad)
 {
 	double *p = new double[2];
-	solution X0(???), X1(???);
+	solution X0(x0), X1(x0 + d);
 	X0.fit_fun(ud, ad);
 	X1.fit_fun(ud, ad);
-	if (???)
+	if (X0.y == X1.y)
 	{
-		???;
-		???;
+		p[0] = X0.x(0);
+		p[1] = X1.x(0);
 		return p;
 	}
-	if (???)
+	if (X0.y < X1.y)
 	{
 		d *= -1;
-		???;
+		X1.x = X0.x + d;
 		X1.fit_fun(ud, ad);
-		if (???)
+		if (X0.y <= X1.y)
 		{
-			???;
-			???;
+			p[0]= X1.x(0);
+			p[1] = X1.x(0) - d;
 			return p;
 		}
 	}
 	solution X2;
-	int i = ???;
+	int i = 0;
 	while (true)
 	{
-		???;
+		X2 = x0 + pow(alpha, i) * d;
+		
 		X2.fit_fun(ud, ad);
-		if (???)
+		if (solution::f_calls > Nmax || X1.y <= X2.y)
 			break;
-		???;
-		???;
+		
+		X0 = X1;
+		X1 = X2;
+
 		++i;
 	}
-	d > 0 ? ??? : ???;
+	//d > 0 ?  : ;
+	if (X0.y < X2.y)
+	{
+		p[0] = X0.x(0);
+		p[1] = X2.x(0);
+	}
+	else
+	{
+		p[0] = X2.x(0);
+		p[1] = X0.x(0);
+	}
 	return p;
 }
 
@@ -51,22 +64,26 @@ solution fib(double a, double b, double epsilon, matrix *ud, matrix *ad)
 	for (int i = 2; i < n; ++i)
 		F[i] = F[i-1] + F[i-2];
 	solution A(a), B(b), C, D;
-	C.x = B.x - F[n-2]/F[n-1] * (B.x - A.x);
-	D.x = A.x + B.x -C.x;
+	C.x = B.x(0) - F[n-2]/F[n-1] * (B.x(0) - A.x(0));
+	D.x = A.x(0) + B.x(0) -C.x(0);
 	C.fit_fun(ud, ad);
 	D.fit_fun(ud, ad);
 	for (int i = 0; i <= n - 3; ++i)
 	{
-		if (C.y < D.y)
-			A.x = A.;
-		else
-			???;
-		C.x = ???;
-		D.x = ???;
+		if (C.y < D.y) {
+            A = A;
+            B = D;
+        }
+		else {
+            B = B;
+            A = C;
+        }
+		C.x = B.x - F[n-i-2]/F[n-i-1] * (B.x - A.x);
+		D.x = A.x + B.x - C.x;
 		C.fit_fun(ud, ad);
 		D.fit_fun(ud, ad);
 #if LAB_NO==2 && LAB_PART==2
-		???;
+		(*ud).add_row((B.x() - A.x()));
 #endif
 	}
 	return C;
@@ -74,54 +91,63 @@ solution fib(double a, double b, double epsilon, matrix *ud, matrix *ad)
 
 solution lag(double a, double b, double epsilon, double gamma, int Nmax, matrix *ud, matrix *ad)
 {
-	solution A(???), B(???), C, D, D_old(a);
-	C.x = ???;
+	solution A(a), B(b), C, D, D_old(a);
+	C.x = (a + b)/2;
 	A.fit_fun(ud, ad);
 	B.fit_fun(ud, ad);
 	C.fit_fun(ud, ad);
 	double l, m;
+    int i = 0;
 	while (true)
 	{
 		l = A.y(0)*(pow(B.x(0), 2) - pow(C.x(0), 2)) + B.y(0)*(pow(C.x(0), 2) - pow(A.x(0), 2)) + C.y(0)*(pow(A.x(0), 2) - pow(B.x(0), 2));
 		m = A.y(0)*(B.x(0) - C.x(0)) + B.y(0)*(C.x(0) - A.x(0)) + C.y(0)*(A.x(0) - B.x(0));
-		if (???)
+		if (m <= 0) // <----- tego gowna nie bylo na schemacie blokowym ale chujnia chlopaki co robimy?
 		{
 			C.x = NAN;
 			C.y = NAN;
 			return C;
 		}
-		D.x = ???;
+		D.x = 0.5 * l/m;
 		D.fit_fun(ud, ad);
-		if (???)
+
+        if (A.x < D.x && A.x < C.x && D.x < C.x)
 		{
-			if (???)
+			if (D.y < C.y)
 			{
-				???;
-				???;
+				A= A;
+				C = D;
+                B = C;
 			}
 			else
-				???;
+				A = D;
+                C = C;
+                B = B;
 		}
-		else if (???)
+		else if (C.x < D.x && C.x < B.x && D.x < B.x)
 		{
-			if (???)
+			if (D.y < C.y)
 			{
-				???;
-				???;
+				A = C;
+				C = D;
+                B = B;
 			}
-			else
-				???;
+			else {
+                A = A;
+                C = C;
+                B = D;
+            }
 		}
-		else
+		else // return error
 		{
 			C.x = NAN;
 			C.y = NAN;
 			return C;
 		}
 #if LAB_NO==2 && LAB_PART==2
-		???
+		(*ud).add_row((B.x() - A.x()));
 #endif
-		if (???)
+		if ( (B.x - A.x < epsilon) || (abs(D.x(1) - D.x(0) <= gamma)) )
 			return C;
 		D_old = D;
 	}
